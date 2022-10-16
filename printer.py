1cc35b478304f488ac684c0f3959c76b1deed397
raidgar98 <raidgar98@onet.pl>
2022-10-16 18:57:25 +0200

    Parametrize file to print

--- printer.py
+++ printer.py
@@ -1,4 +1,8 @@
-file_name = 'example.test.txt'
+from sys import argv
+
+DEFAULT_FILE_NAME = 'example.test.txt'
+file_name = argv[1] if len(argv) > 1 else DEFAULT_FILE_NAME
+
 with open(file_name, 'r') as file:
 	for line in file:
 		print(line)
